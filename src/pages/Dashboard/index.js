8b11eb28b3c8715e36a4957114cf95de972fc2b7
Diego Souza <dee-souza@hotmail.com>
2019-09-09 16:46:21 -0300

    Feat: Dashboard page create

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,49 @@
-import React from 'react';
-import api from '~/services/api';
+import React, { useState, useMemo } from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-	api.get('appointments');
-	return <h1>Dashboard</h1>;
+	const [date, setDate] = useState(new Date());
+
+	const dateFormatted = useMemo(
+		() => format(date, "d 'de' MMMM", { locale: pt }),
+		[date]
+	);
+
+	function handlePrevDay() {
+		setDate(subDays(date, 1));
+	}
+
+	function handleNextDay() {
+		setDate(addDays(date, 1));
+	}
+
+	return (
+		<Container>
+			<header>
+				<button type="button" onClick={handlePrevDay}>
+					<MdChevronLeft size={36} color="#FFF" />
+				</button>
+				<strong>{dateFormatted}</strong>
+				<button type="button" onClick={handleNextDay}>
+					<MdChevronRight size={36} color="#FFF" />
+				</button>
+			</header>
+
+			<ul>
+				<Time past>
+					<strong>08:00</strong>
+					<span>Diego Souza</span>
+				</Time>
+
+				<Time available>
+					<strong>09:00</strong>
+					<span>Em Aberto</span>
+				</Time>
+			</ul>
+		</Container>
+	);
 }
