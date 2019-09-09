130f4471a2d1a20c0673963010079e4c1a976690
Diego Souza <dee-souza@hotmail.com>
2019-09-09 14:01:28 -0300

    Feat: Change and save image profile user

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '~/assets/logo-purple.svg';
@@ -7,6 +8,8 @@ import { Container, Content, Profile } from './styles';
 import Notifications from '~/components/Notifications';
 
 export default function Header() {
+	const profile = useSelector(state => state.user.profile);
+
 	return (
 		<Container>
 			<Content>
@@ -19,11 +22,14 @@ export default function Header() {
 					<Notifications />
 					<Profile>
 						<div>
-							<strong>Diego Souza</strong>
+							<strong>{profile.name}</strong>
 							<Link to="/profile">MEU PERFIL</Link>
 						</div>
 						<img
-							src="https://api.adorable.io/avatars/50/abott@adorable.png"
+							src={
+								profile.avatar.url ||
+								'https://api.adorable.io/avatars/50/abott@adorable.png'
+							}
 							alt="Diego"
 						/>
 					</Profile>
